795d49f5c961ff1086cf7b1ce4fe180cae4603a7
Jeremie <jeremie@allisone.ai>
2021-11-04 18:10:15 +0100

    feat: add port listening

---
+++ src/Listener.class.cpp
@@ -0,0 +1,157 @@
+#include "Listener.class.hpp"
+
+// CTORS / DTORS
+
+Listener::Listener(void) throw(std::runtime_error)
+{
+  socklen_t sockaddrSize = sizeof(struct sockaddr);
+  bzero(&this->socket_in, sockaddrSize);
+  this->socket_in.sin_family = AF_INET;
+  this->socket_in.sin_addr.s_addr = htonl(INADDR_ANY);
+  this->socket_in.sin_port = htons(PORT);
+
+  this->socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+  if (this->socket_fd == -1)
+    throw std::runtime_error("socket() failed");
+
+  int opt = 1;
+  if (setsockopt(this->socket_fd, SOL_SOCKET, SO_REUSEADDR, &opt,
+                 sizeof(opt)) != 0)
+    throw std::runtime_error("setsockopt() failed");
+
+  if (bind(this->socket_fd, (struct sockaddr *)&this->socket_in, sockaddrSize) == -1)
+    throw std::runtime_error("bind() failed");
+
+  bzero(this->clients, sizeof(this->clients));
+}
+
+Listener::~Listener(void)
+{
+  close(this->socket_fd);
+}
+
+// OPERATORS
+
+// MEMBER FUNCTIONS
+
+int Listener::updateSockets(void)
+{
+  int max_fd = this->socket_fd;
+
+  // FD_ZERO() efface un ensemble.
+  FD_ZERO(&this->read_fds);
+
+  // FD_SET() ajoute un descripteur de fichier dans un ensemble.
+  FD_SET(this->socket_fd, &this->read_fds);
+
+  //add child sockets to set
+  for (int i = 0; i < MAX_CLIENTS; i++)
+  {
+    int client_sock_fd = this->clients[i];
+
+    //if valid socket descriptor then add to read list
+    if (client_sock_fd > 0)
+      FD_SET(client_sock_fd, &this->read_fds);
+    if (client_sock_fd > max_fd)
+      max_fd = client_sock_fd;
+  }
+
+  return max_fd;
+}
+
+int Listener::registerNewSocket(const int client_socket_fd)
+{
+  //add new socket to array of sockets
+  for (int i = 0; i < MAX_CLIENTS; i++)
+  {
+    //if position is empty
+    if (this->clients[i] == 0)
+    {
+      this->clients[i] = client_socket_fd;
+      Tintin_reporter::log("...registered the new connection");
+      printf("Adding to list of sockets as %d\n", i);
+      return i;
+    }
+  }
+  return -1;
+}
+
+void Listener::handleNewConnection(void) throw(std::runtime_error)
+{
+  socklen_t sockaddrSize = sizeof(struct sockaddr);
+  struct sockaddr_in clientSocketAddress;
+  int client_socket_fd;
+
+  // FD_ISSET() vérifie si un descripteur de fichier est contenu dans un ensemble, principalement utile après le retour de select().
+  if (FD_ISSET(this->socket_fd, &this->read_fds))
+  {
+    if ((client_socket_fd = accept(
+             this->socket_fd,
+             (struct sockaddr *)&clientSocketAddress,
+             (socklen_t *)&sockaddrSize)) < 0)
+      throw std::runtime_error("accept() failed");
+
+    Tintin_reporter::log("Incoming connection...");
+
+    if (this->registerNewSocket(client_socket_fd) < 0)
+      Tintin_reporter::error("...cannot register the new connection (no space left)");
+  }
+}
+
+void Listener::handleSockOperation(void)
+{
+  char buffer[BUFFER_SIZE];
+
+  for (int i = 0; i < MAX_CLIENTS; i++)
+  {
+    const int client_sock_fd = this->clients[i];
+
+    if (client_sock_fd == 0)
+      continue;
+
+    if (FD_ISSET(client_sock_fd, &this->read_fds))
+    {
+      // Check if it was for closing , and also read the
+      // incoming message
+      int read_bytes = recv(client_sock_fd, buffer, BUFFER_SIZE, 0);
+
+      if (read_bytes <= 0)
+      {
+        // Close the socket and mark as 0 in list for reuse
+        close(client_sock_fd);
+        this->clients[i] = 0;
+        Tintin_reporter::log("Close user connection");
+      }
+
+      // Echo back the message that came in
+      else
+      {
+        // set the string terminating NULL byte on the end
+        // of the data read
+        buffer[read_bytes] = '\0';
+        const std::string client_msg(buffer);
+        Tintin_reporter::log("User input:" + client_msg);
+      }
+    }
+  }
+}
+
+void Listener::listen(void) throw(std::runtime_error)
+{
+  if (::listen(this->socket_fd, MAX_CLIENTS) == -1)
+    throw std::runtime_error("listen() failed");
+
+  while (true)
+  {
+    const int max_fd = this->updateSockets();
+
+    int activity = select(max_fd + 1, &this->read_fds, NULL, NULL, NULL);
+    if ((activity < 0) && (errno != EINTR))
+      throw std::runtime_error("select() failed");
+
+    this->handleNewConnection();
+    this->handleSockOperation();
+  }
+}
+
+// NON-MEMBER FUNCTIONS
